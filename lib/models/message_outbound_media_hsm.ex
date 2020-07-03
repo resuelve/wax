77590458089f08766f887eb79a6fa1a37dd710d5
Neftaly Luis <neftaly.luis@resuelve.mx>
2020-07-03 13:27:36 -0500

    Adds support for Base64 payloads on Media templates

--- lib/models/message_outbound_media_hsm.ex
+++ lib/models/message_outbound_media_hsm.ex
@@ -38,9 +38,15 @@ defmodule Whatsapp.Models.MessageOutboundMediaHsm do
   """
   @spec new(Keyword.t()) :: __MODULE__.t()
   def new(options) do
-    attrs = Enum.into(options, Map.new())
+    attrs =
+      options
+      |> Enum.into(Map.new())
+      |> Map.merge(@default_values)
+      |> _add_extension_from_mime()
+      |> _add_caption()
+      |> _add_binary_data()
 
-    Kernel.struct(__MODULE__, Map.merge(@default_values, attrs))
+    Kernel.struct(__MODULE__, attrs)
   end
 
   def set_media_id(media, media_id) do
@@ -136,5 +142,58 @@ defmodule Whatsapp.Models.MessageOutboundMediaHsm do
         ]
       }
     }
+  end
+
+  @doc """
+  Agrega el binario del archivo media al campo de data
+  """
+  @spec _add_binary_data(map()) :: map()
+  def _add_binary_data(%{data: binary_data} = msg) when is_bitstring(binary_data) do
+    binary_data =
+      if String.starts_with?(binary_data, "data:") do
+        [_, binary_data] = String.split(binary_data, ",", parts: 2)
+        Base.decode64!(binary_data)
+      else
+        binary_data
+      end
+
+    %{msg | data: binary_data}
+  end
+
+  def _add_binary_data(_), do: {:error, "Invalid data"}
+
+  # Obtiene la extension del archivo a partir del mime_type
+  # El null (o vacío) a PDF es por un bug de Whatsapp
+  @spec _add_extension_from_mime(map()) :: map()
+  defp _add_extension_from_mime(%{mime_type: nil} = msg) do
+    Map.put(msg, :extension, "pdf")
+  end
+
+  defp _add_extension_from_mime(%{mime_type: mime_type} = msg) do
+    [_, extension] = Regex.run(~r/\/([a-z0-9]+)/, mime_type)
+    Map.put(msg, :extension, extension)
+  end
+
+  defp _add_extension_from_mime(msg) do
+    Map.put(msg, :extension, "pdf")
+  end
+
+  # Asigna el nombre del archivo como content para los mensajes media de tipo document
+  @spec _add_caption(map()) :: map()
+  def _add_caption(%{caption: caption, type: "document", file_name: file_name} = msg)
+      when caption in [nil, ""] do
+    Map.put(msg, :caption, file_name)
+  end
+
+  def _add_caption(%{caption: caption, type: "document", extension: extension} = msg) do
+    Map.put(msg, :caption, caption <> "." <> extension)
+  end
+
+  def _add_caption(%{caption: caption} = msg) do
+    Map.put(msg, :caption, caption)
+  end
+
+  def _add_caption(_) do
+    {:error, "Invalid caption"}
   end
 end
