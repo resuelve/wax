d407892eb4802775ac929868ae1c7d3bf787840f
Alfonso Martinez <poncho@gobravo.io>
2025-06-27 01:19:52 +0200

    Adds tests of sending image messages

--- test/cloud_api/messages_test.exs
+++ test/cloud_api/messages_test.exs
@@ -1,32 +1,31 @@
 defmodule Wax.CloudAPI.MessagesTest do
   use Whatsapp.Case
 
-  alias Wax.CloudAPI.{Auth, Messages}
+  alias Wax.CloudAPI.{Auth, Media, Messages}
   alias Wax.Messages.Message
 
   setup do
     bypass = Bypass.open()
-    Application.put_env(:wax, :cloud_api_url, "http://localhost:#{bypass.port}")
+    Application.put_env(:wax, :cloud_api, url: "http://localhost:#{bypass.port}", version: "")
 
+    test_to_number = "TEST550000000001"
     test_wa_number_id = "TEST0000000001"
     test_token = "TESTTOKEN999"
 
     auth = Auth.new(test_wa_number_id, test_token)
 
-    {:ok, bypass: bypass, auth: auth}
+    {:ok, bypass: bypass, auth: auth, to: test_to_number}
   end
 
   describe "Text messages" do
-    test "Send a text message", %{bypass: bypass, auth: auth} do
-      test_to_number = "TEST550000000001"
-
+    test "Send a text message", %{bypass: bypass, auth: auth, to: to} do
       Bypass.expect_once(bypass, "POST", "/#{auth.whatsapp_number_id}/messages", fn conn ->
         response = ~s<{"messaging_product": "whatsapp", "messages": [{"id": "TESTMESSAGEID"}]}>
         Plug.Conn.resp(conn, 200, response)
       end)
 
       message =
-        test_to_number
+        to
         |> Message.new()
         |> Message.set_type(:text)
         |> Message.set_text("Text message test")
@@ -34,22 +33,18 @@ defmodule Wax.CloudAPI.MessagesTest do
       assert {:ok, %{"messages" => [%{"id" => "TESTMESSAGEID"}]}} = Messages.send(message, auth)
     end
 
-    test "Cannot send a text message with no text", %{auth: auth} do
-      test_to_number = "TEST550000000001"
-
+    test "Cannot send a text message with no text", %{auth: auth, to: to} do
       message =
-        test_to_number
+        to
         |> Message.new()
         |> Message.set_type(:text)
 
       assert {:error, _error} = Messages.send(message, auth)
     end
 
-    test "Cannot send a text message with an empty string", %{auth: auth} do
-      test_to_number = "TEST550000000001"
-
+    test "Cannot send a text message with an empty string", %{auth: auth, to: to} do
       message =
-        test_to_number
+        to
         |> Message.new()
         |> Message.set_type(:text)
         |> Message.set_text("")
@@ -58,12 +53,44 @@ defmodule Wax.CloudAPI.MessagesTest do
     end
   end
 
-  describe "Various errors" do
-    test "Sending a message of an unsupported type", %{auth: auth} do
-      test_to_number = "TEST550000000001"
+  describe "Image messages" do
+    test "Send an image message", %{bypass: bypass, auth: auth, to: to} do
+      Bypass.expect_once(bypass, "POST", "/#{auth.whatsapp_number_id}/media", fn conn ->
+        response = ~s<{"id": "TEST00000000"}>
+        Plug.Conn.resp(conn, 200, response)
+      end)
+
+      {:ok, media_id} =
+        [extname: "png"]
+        |> Briefly.create!()
+        |> Media.upload(auth)
+
+      Bypass.expect(bypass, "POST", "/#{auth.whatsapp_number_id}/messages", fn conn ->
+        response = ~s<{"messaging_product": "whatsapp", "messages": [{"id": "TESTMESSAGEID"}]}>
+        Plug.Conn.resp(conn, 200, response)
+      end)
 
       message =
-        test_to_number
+        to
+        |> Message.new()
+        |> Message.set_type(:image)
+
+      message_with_caption = Message.add_image(message, media_id, "Test caption")
+
+      assert {:ok, %{"messages" => [%{"id" => "TESTMESSAGEID"}]}} =
+               Messages.send(message_with_caption, auth)
+
+      message_no_caption = Message.add_image(message, media_id)
+
+      assert {:ok, %{"messages" => [%{"id" => "TESTMESSAGEID"}]}} =
+               Messages.send(message_no_caption, auth)
+    end
+  end
+
+  describe "Various errors" do
+    test "Sending a message of an unsupported type", %{auth: auth, to: to} do
+      message =
+        to
         |> Message.new()
         |> Message.set_type(:contact)
         |> Message.set_text("")
