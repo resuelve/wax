c9e0138e2f8e9f77a729e60e40fe451d051e4633
Alfonso Martinez <poncho@gobravo.io>
2025-08-08 15:38:39 +0200

    Adds tests

--- test/cloud_api/media_test.exs
+++ test/cloud_api/media_test.exs
@@ -35,4 +35,28 @@ defmodule Wax.CloudAPI.MediaTest do
     assert {:error, error} = Media.upload_from_path(test_file, auth)
     assert String.contains?(error, "extension")
   end
+
+  test "Upload a document file from a binary", %{bypass: bypass, auth: auth} do
+    media_id = "TEST00000000"
+
+    Bypass.expect_once(bypass, "POST", "/#{auth.whatsapp_number_id}/media", fn conn ->
+      response = ~s<{"id": "#{media_id}"}>
+      Plug.Conn.resp(conn, 200, response)
+    end)
+
+    test_file = Briefly.create!(extname: ".pdf")
+    binary_data = File.read!(test_file)
+
+    assert {:ok, ^media_id} = Media.upload_binary(binary_data, test_file, auth)
+  end
+
+  test "Uploading a document from a binary with no extension should return an error", %{
+    auth: auth
+  } do
+    test_file = Briefly.create!(extname: "")
+    binary_data = File.read!(test_file)
+
+    assert {:error, error} = Media.upload_binary(binary_data, test_file, auth)
+    assert String.contains?(error, "extension")
+  end
 end
