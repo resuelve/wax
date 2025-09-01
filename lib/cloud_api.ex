c9d8f0b972008855d243ad1e7479cbcf6d680139
Alfonso Martinez <poncho@gobravo.io>
2025-09-01 17:14:59 +0200

    Adds support to build URLs without a whatsapp ID
    This is required for downloading media because the URL is just /{MEDIA_ID}

--- lib/cloud_api.ex
+++ lib/cloud_api.ex
@@ -8,12 +8,22 @@ defmodule Wax.CloudAPI do
   """
   @spec build_url(String.t(), String.t()) :: String.t()
   def build_url(whatsapp_number_id, endpoint) do
+    whatsapp_number_id
+    |> Path.join(endpoint)
+    |> do_build_url()
+  end
+
+  def build_url(endpoint) do
+    do_build_url(endpoint)
+  end
+
+  defp do_build_url(endpoint) do
     config = Application.get_env(:wax, :cloud_api)
     url = config[:url]
     api_version = config[:api_version]
 
     url
-    |> URI.merge("#{api_version}/#{whatsapp_number_id}/#{endpoint}")
+    |> URI.merge("#{api_version}/#{endpoint}")
     |> URI.to_string()
   end
 end
